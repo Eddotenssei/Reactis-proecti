ead2e2adf3f770e8c423ea0b73df2a6ef340a0dc
Ilia Chichikoshvili <eddotenssei@gmail.com>
2025-10-26 13:27:03 +0400

    added style to reset button

--- vite-project/src/components/ResetButton.jsx
+++ vite-project/src/components/ResetButton.jsx
@@ -1,3 +1,15 @@
 export default function ResetButton({ title, handleReset }) {
-  return <button onClick={handleReset}>{title}</button>;
+  return (
+    <button
+      style={{
+        margin: "5px",
+        padding: "10px 20px",
+        cursor: "pointer",
+        borderRadius: "8px",
+      }}
+      onClick={handleReset}
+    >
+      {title}
+    </button>
+  );
 }
