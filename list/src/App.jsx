544820457280c32fa0a838c2f08a5f9f08911903
shekeladzeniko <shekeladzenikoloz95@gmail.com>
2025-11-19 15:59:48 +0400

    davalebis dawera

--- list/src/App.jsx
+++ list/src/App.jsx
@@ -1,16 +1,38 @@
+import { useState } from "react";
+import "./App.css";
+import { nanoid } from "nanoid";
 export default function App() {
-  const fruits = [
-    { name: "Apple", calories: 95 },
-    { name: "Banana", calories: 105 },
-    { name: "Cherry", calories: 50 },
-    { name: "Pineapple", calories: 452 },
-    { name: "Elderberry", calories: 73 },
+  
+  const [todos, setTodos] = useState([])
+  
+  const presetTodos = [
+    { id: 1, text: "Preset 1" },
+    { id: 2, text: "Preset 2" },
+    { id: 3, text: "Preset 3" }
   ];
 
-  const modified = fruits.map((f) => (
-    <li>
-      fruit {f.name} has {f.calories} calories.
-    </li>
-  ));
-  return <ol>{modified}</ol>;
-}
+
+  addTodo = ()=>{
+      const newTodo = {
+      id: nanoid(),
+      text: todoText,
+    };
+  }
+  
+  deleteTodo = (id) => {
+     const filtered = todos.filter((todo) => todo.id !== id);
+    setTodos(filtered);
+  }
+
+
+
+
+    setTodos([...todos, newTodo])
+  }
+
+  return(
+    <>
+    
+    </>
+  );
+
