243ad6cb937e26b9e70415a059b2e099b0a1f3e1
Ilia Chichikoshvili <eddotenssei@gmail.com>
2026-02-18 15:28:05 +0400

    dasdasdas

--- posts/src/pages/PostDetails.jsx
+++ posts/src/pages/PostDetails.jsx
@@ -1,7 +1,37 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router";
+import { Link } from "react-router";
 
 export default function PostDetails() {
   const { id } = useParams();
+    const [posts, setPosts] = useState([]);
 
-  return <div>This is Posts Details</div>;
+
+
+
+    useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          `https://jsonplaceholder.typicode.com/posts/${id}`,
+        );
+        const data = await response.json();
+        setPosts(data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    fetchData();
+  },[id]);
+
+
+
+  return (
+    <div>
+      <h1>{posts.title}</h1>
+      <p>{posts.body}</p>
+      <button><Link to={`/`}>main page</Link></button>
+    </div>
+  );
 }
